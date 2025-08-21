f46996531ff5bc58d5a7adbf659e9b6338b6fa6c
u1000e <73210038+u1000e@users.noreply.github.com>
2025-08-21 11:05:08 +0900

    Modify TestClass

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -3,9 +3,11 @@ package com.kh.network.run;
 public class TestClass {
 	
 	public static void main(String[] args) {
-		
+		// 얘는 깜장이
 		
 		// 이번엔 진짜로 확정 지ㅓㅇ 볼거임
+		// Home Sweet Home ~ ~ ~
+		// 집좋다 좋아 ~~ 이게 집이지~~~
 	}
 
 }
