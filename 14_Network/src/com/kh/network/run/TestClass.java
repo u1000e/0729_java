5e73a927f1923fb1caac516305ace4a59bf7f45c
u1000e <73210038+u1000e@users.noreply.github.com>
2025-08-21 11:43:38 +0900

    end to git

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -42,6 +42,18 @@ public class TestClass {
 		System.out.println(3);
 	}
 	
+	public void pull() {
+		System.out.println("Pull을 잘하자");
+	}
+	
+	public void printStatement() {
+		System.out.println("1. 워크스페이스에서 gitBash를 실행한다.");
+		System.out.println("2. git pull origin master");
+		System.out.println("3. git add .");
+		System.out.println("4. git commit -m 뭐했는지");
+		System.out.println("5. git push origin master");
+	}
+	
 	
 
 	
